ccb743f25fa621bf9c00c3723445a86b104a3d8c
rayaneo92 <80986824+rayaneo92@users.noreply.github.com>
2021-03-23 23:34:04 +0100

    Add files via upload
    projet quizz nba  2 questions

---
+++ projet quizz nba.py
@@ -0,0 +1,48 @@
+
+
+# QUESTIONS
+NBA = [ " Quelle est l''équipe a gagné le plus de titres NBA ? " ]
+NBA1 = [" Qui est le meilleur scoreur de la ligue ? (ALL TIME)  " ]
+
+rp1 = [" 1 :Boston Celtics " ]
+rp2 =[" 1 : Micheal Jordan " ]
+
+rp11 = [" 2 : L.A Lakers " ]
+rp111 = [" 2 : Kobe Bryant"  ]
+
+rp3 = [" 3 : Brooklyn Nets " ]
+rp33 = [" 3  : Kareem Abdul Jabbar" ]
+
+
+
+# QUESTION 2
+
+print(NBA)
+print(rp1)
+print(rp11)
+print(rp3)
+
+rep= int(input ("Entrez LA bonne réponse :  " ))
+rpcorrecte = 2
+
+if rep != rpcorrecte:
+    print("FAUX")
+else :
+    print("Bien")
+
+# QUESTION 2
+
+print(NBA1)
+print(rp2)
+print(rp111)
+print(rp33)
+
+
+reputilisateur = int(input ("Entrez LA bonne réponse :  " ))
+rpcorrecte = 3
+
+
+if reputilisateur != rpcorrecte:
+    print("FAUX")
+else :
+    print("Bien")
